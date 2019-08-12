89d9eac901281a5f68ad2b885e77f7965c2b6991
É isso ai gordox < isso ai gordox@computador>
2019-08-12 12:11:01 -0300

    Gerente

---
+++ SistemaHospital/src/br/com/hospitalif/model/Gerente.java
@@ -0,0 +1,12 @@
+package br.com.hospitalif.model;
+
+public class Gerente {
+	private String cargo;
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+}
