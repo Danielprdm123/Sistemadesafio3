ed121e14a6cc5af62eb7505bfd3745c07004bd94
É isso ai gordox < isso ai gordox@computador>
2019-08-12 12:37:01 -0300

    Enfermeiro

---
+++ SistemaHospital/src/br/com/hospitalif/model/Enfermeiro.java
@@ -0,0 +1,12 @@
+package br.com.hospitalif.model;
+
+public class Enfermeiro extends Funcionario {
+	private int numeroderegistro;
+
+	public void setNumeroderegistro(int numeroderegistro) {
+		this.numeroderegistro = numeroderegistro;
+	}
+	public int getNumeroderegistro() {
+		return numeroderegistro;
+	}
+}
