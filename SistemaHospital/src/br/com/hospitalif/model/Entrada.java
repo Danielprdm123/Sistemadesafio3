919ef83a3c65ee81f14216d387b6aa4632ec5719
É isso ai gordox < isso ai gordox@computador>
2019-08-12 17:03:27 -0300

    1208

---
+++ SistemaHospital/src/br/com/hospitalif/model/Entrada.java
@@ -0,0 +1,50 @@
+package br.com.hospitalif.model;
+
+import java.awt.List;
+import java.sql.Date;
+
+public class Entrada {
+	private int idEntrada;
+	private Date dataEntrada;
+	private Date dataDeSaida;
+	private String statusDeEntrada;
+	private List situacaoDePaciente;
+	
+	public void setIdEntrada(int idEntrada) {
+		this.idEntrada = idEntrada;
+	}
+	public int getIdEntrada() {
+		return idEntrada;
+	}
+
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+	
+	public void setDataDeSaida(Date dataDeSaida) {
+		this.dataDeSaida = dataDeSaida;
+	}
+	public Date getDataDeSaida() {
+		return dataDeSaida;
+	}
+	
+	public void setStatusDeEntrada(String statusDeEntrada) {
+		this.statusDeEntrada = statusDeEntrada;
+	}
+	public String getStatusDeEntrada() {
+		return statusDeEntrada;
+	}
+	
+	public void setSituacaoDePaciente(List situacaoDePaciente) {
+		this.situacaoDePaciente = situacaoDePaciente;
+	}
+	public List getSituacaoDePaciente() {
+		return situacaoDePaciente;
+	}
+	
+	
+
+}
