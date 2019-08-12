698bbff05e667e6439bece166e235b5d975efb64
É isso ai gordox < isso ai gordox@computador>
2019-08-12 12:55:39 -0300

    Enfermidade

---
+++ SistemaHospital/src/br/com/hospitalif/model/Enfermidade.java
@@ -0,0 +1,38 @@
+package br.com.hospitalif.model;
+
+public class Enfermidade {
+	private int idEnfermidade;
+	private String nome;
+	private String tipo;
+	private String descricao;
+	public void setIdEnfermidade(int idEnfermidade) {
+		this.idEnfermidade = idEnfermidade;
+	}
+	public int getIdEnfermidade() {
+		return idEnfermidade;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+
+
+}
