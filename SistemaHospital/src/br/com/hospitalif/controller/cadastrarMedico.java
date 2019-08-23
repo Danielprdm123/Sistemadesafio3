419fc0117780468b94bf176cd9a161fe414b5e74
É isso ai gordox < isso ai gordox@computador>
2019-08-23 19:19:29 -0300

    hoje

---
+++ SistemaHospital/src/br/com/hospitalif/controller/cadastrarMedico.java
@@ -0,0 +1,21 @@
+package br.com.hospitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class cadastrarMedico {
+
+    @FXML
+    private TextField txtNumregistro;
+
+    @FXML
+    private TextField txtEspecialidade;
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private Button btnVolta;
+
+}
