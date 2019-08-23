898079f9a1b072a38bf378a11bffc87803d555b2
07209702105 <07209702105@192.168.56.1>
2019-08-23 00:14:09 -0400

    aula

---
+++ SistemaHospital/src/br/com/hospitalif/controller/cadastrarPaciente.java
@@ -0,0 +1,24 @@
+package br.com.hospitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class cadastrarPaciente {
+
+    @FXML
+    private ListView<?> txtHistorico;
+
+    @FXML
+    private ListView<?> txtDoenca;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private Button btnCadastrar;
+
+ 
+
+}
