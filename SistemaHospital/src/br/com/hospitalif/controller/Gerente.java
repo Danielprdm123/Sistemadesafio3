3d06756e0be0697873aba4522c2704b28c1fd48a
É isso ai gordox < isso ai gordox@computador>
2019-08-23 19:16:24 -0300

    hoje

---
+++ SistemaHospital/src/br/com/hospitalif/controller/Gerente.java
@@ -0,0 +1,18 @@
+package br.com.hospitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Gerente {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private TextField txtCargo;
+
+    @FXML
+    private Button btnVoltar;
+
+}
