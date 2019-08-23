b04256be7f887a90be2cefd228e988a5727f138b
07209702105 <07209702105@192.168.56.1>
2019-08-22 23:40:40 -0400

    aula

--- SistemaHospital/src/br/com/hospitalif/controller/DS.java
+++
@@ -1,5 +0,0 @@
-package br.com.hospitalif.controller;
-
-public class DS {
-
-}
---
+++ SistemaHospital/src/br/com/hospitalif/controller/cadastroPessoa.java
@@ -0,0 +1,57 @@
+package br.com.hospitalif.controller;
+
+public class cadastroPessoa {
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.RadioButton;
+	import javafx.scene.control.TextField;
+	import javafx.scene.control.ToggleGroup;
+	import javafx.scene.text.Text;
+
+	public class cadastroPessoa {
+
+	    @FXML
+	    private Text lblPessoa;
+
+	    @FXML
+	    private TextField txtStatus;
+
+	    @FXML
+	    private Button btnCadastrar;
+
+	    @FXML
+	    private TextField textSangue;
+
+	    @FXML
+	    private TextField txtNome;
+
+	    @FXML
+	    private TextField txtCpf;
+
+	    @FXML
+	    private ToggleGroup sexo;
+
+	    @FXML
+	    private TextField txtIdade;
+
+	    @FXML
+	    private RadioButton cboSexo;
+
+	    @FXML
+	    void 00000079(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void salvarPessoa(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void f8f8ffba(ActionEvent event) {
+
+	    }
+
+	}
+
+}
