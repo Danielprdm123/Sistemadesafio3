d39b028e3092f0a894ae4494c9b1ffa35641ba04
07209702105 <07209702105@192.168.56.1>
2019-08-24 02:31:49 -0400

    vai

---
+++ SistemaHospital/src/br/com/hospitalif/controller/Atendente.java
@@ -0,0 +1,23 @@
+package br.com.hospitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Atendente {
+
+    @FXML
+    private Button btnCadastarpaciente;
+
+    @FXML
+    private Button btnVoltar;
+
+
+    @FXML
+    void cadastraratendente(ActionEvent event) {
+
+    }
+
+
+
+}
