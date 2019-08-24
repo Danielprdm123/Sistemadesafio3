4bd9ded46714d006d023b03529cd77a4a48e8899
07209702105 <07209702105@192.168.56.1>
2019-08-24 02:20:59 -0400

    vai

---
+++ SistemaHospital/src/br/com/hospitalif/controller/Enfermeiro.java
@@ -0,0 +1,20 @@
+package br.com.hospitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Enfermeiro {
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextField txtNumres;
+
+    @FXML
+    private Button btnEnvirar;
+
+
+
+}
