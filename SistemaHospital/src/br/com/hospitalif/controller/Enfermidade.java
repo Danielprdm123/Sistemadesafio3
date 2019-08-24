2c5a14c9814af5d8d96a1d7246988e2196df47d0
07209702105 <07209702105@192.168.56.1>
2019-08-24 02:16:56 -0400

    vai

---
+++ SistemaHospital/src/br/com/hospitalif/controller/Enfermidade.java
@@ -0,0 +1,26 @@
+package br.com.hospitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Enfermidade {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private TextField txtTipo;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextArea txtDesc;
+
+}
+
