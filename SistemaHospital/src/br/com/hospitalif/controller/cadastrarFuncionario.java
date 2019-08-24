0566bf4e21fc1b6f4f4cc778d371a3d9738fb9a5
07209702105 <07209702105@192.168.56.1>
2019-08-24 01:43:10 -0400

    vai

---
+++ SistemaHospital/src/br/com/hospitalif/controller/cadastrarFuncionario.java
@@ -0,0 +1,28 @@
+package br.com.hospitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class cadastrarFuncionario {
+
+    @FXML
+    private TextField txtLogin;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private TextArea txtStatus;
+
+    @FXML
+    private Button btnVoltar;
+
+
+}
+
