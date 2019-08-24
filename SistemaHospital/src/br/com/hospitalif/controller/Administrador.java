10c8e2a2723cad89b10bc3d3dc30e29a4980b52f
07209702105 <07209702105@192.168.56.1>
2019-08-24 02:37:54 -0400

    vai

---
+++ SistemaHospital/src/br/com/hospitalif/controller/Administrador.java
@@ -0,0 +1,34 @@
+package br.com.hospitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Administrador {
+
+    @FXML
+    private Button btnCadastrarFuncionario;
+
+    @FXML
+    private Button btnvoltar;
+
+  
+
+    @FXML
+    void CadastrarFuncionario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void f8f8ffba(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltadash(ActionEvent event) {
+
+    }
+
+ 
+
+}
