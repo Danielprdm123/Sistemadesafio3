cd3575d550865772fc337f975a8e3baf3f5bc33b
07209702105 <07209702105@192.168.56.1>
2019-08-24 02:27:54 -0400

    vai

---
+++ SistemaHospital/src/br/com/hospitalif/controller/Dash.java
@@ -0,0 +1,105 @@
+package br.com.hospitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+public class Dash {
+
+    @FXML
+    private Hyperlink linkPaciente;
+
+    @FXML
+    private Hyperlink linkPessoa;
+
+    @FXML
+    private Hyperlink linkEntrada;
+
+    @FXML
+    private Hyperlink linkAtendente;
+
+    @FXML
+    private Hyperlink linkEnfermidadePessoal;
+
+    @FXML
+    private Hyperlink linkEnfermidade;
+
+    @FXML
+    private Hyperlink linkEnfermeiro;
+
+    @FXML
+    private Hyperlink linkAtendimento;
+
+    @FXML
+    private Hyperlink linkAdm;
+
+    @FXML
+    private Hyperlink linkFuncionario;
+
+    @FXML
+    private Hyperlink linkGerente;
+
+    @FXML
+    private Hyperlink linkMedico;
+
+    @FXML
+    void Enfermeiro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Enfermidade(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EnfermidadePessoal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Entrada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Funcionario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Gerente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Atendente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Medico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Paciente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Pessoa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Administrador(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Atendimento(ActionEvent event) {
+
+    }
+
+}
