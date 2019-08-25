4e5ddbf8183beaf090ae854f099605f4c625b1cf
É isso ai gordox < isso ai gordox@computador>
2019-08-25 13:36:48 -0300

    mais um

--- SistemaHospital/src/br/com/hospitalif/controller/Medico.java
+++ SistemaHospital/src/br/com/hospitalif/controller/Medico.java
@@ -4,21 +4,29 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class cadastrarMedico {
-
-    @FXML
-    private Button btnEnviar;
-
-    @FXML
-    private TextField txtEspecialidade;
+public class Medico {
 
     @FXML
     private TextField txtNumregistro;
 
     @FXML
+    private TextField txtEspecialidade;
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
     private Button btnVolta;
 
-  
+    @FXML
+    void Enviarmedico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Voltardash(ActionEvent event) {
+
+    }
 
 }
 
