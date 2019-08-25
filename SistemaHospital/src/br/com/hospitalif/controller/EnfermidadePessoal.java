1e75a0e0f46f0ba194cbab4db3c6b27e6dc30049
É isso ai gordox < isso ai gordox@computador>
2019-08-25 12:21:21 -0300

    mais um

---
+++ SistemaHospital/src/br/com/hospitalif/controller/EnfermidadePessoal.java
@@ -0,0 +1,32 @@
+package br.com.hospitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class EnfermidadePessoal {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextArea txtStatusEnfermidade;
+
+    @FXML
+    private TextArea txtComentario;
+
+    @FXML
+    void EnviarEnfermidadePessoal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarDash(ActionEvent event) {
+
+    }
+
+}
+
