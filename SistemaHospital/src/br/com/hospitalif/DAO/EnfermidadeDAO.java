e07425f5c2f219e7bf1e9b359b354b6c9f8b21aa
É isso ai gordox < isso ai gordox@computador>
2019-09-26 02:02:35 -0300

    vamos

---
+++ SistemaHospital/src/br/com/hospitalif/DAO/EnfermidadeDAO.java
@@ -0,0 +1,58 @@
+package br.com.hospitalif.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.hospitalif.conexao.Conexao;
+import br.com.hospitalif.model.Enfermidade;
+
+public class EnfermidadeDAO {
+	public void save(Enfermidade e) throws SQLException {
+		Conexao conn = new Conexao();
+		Connection conexao = conn.getConnection();
+		System.out.println(conn.getStatus());
+		String sqlInsere = "insert into Enfermidade VALUES(?,?,?,?)";
+		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
+			stmt.setInt(1,e.getIdEnfermidade());
+			stmt.setString(2,e.getNome());
+			stmt.setString(3,e.getTipo());
+			stmt.setString(4,e.getDescricao());
+			stmt.execute();
+	}
+	public void removeID(int id) throws SQLException {
+		Conexao conn = new Conexao();
+		Connection conexao = conn.getConnection();
+		System.out.println(conn.getStatus());
+		String sqlInsere = "DELETE FROM EnfermidadePessoal WHERE id(?)";	
+		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
+			stmt.setInt(1,id);	
+			stmt.execute();
+	}
+
+	public void selecionar(Enfermidade e) throws SQLException {
+		Conexao conn = new Conexao();
+		Connection conexao = conn.getConnection();
+		System.out.println(conn.getStatus());
+		String sqlInsere = "SELECT FROM Enfermidade(?,?,?,?)";
+		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
+			stmt.setInt(1,e.getIdEnfermidade());
+			stmt.setString(2,e.getNome());
+			stmt.setString(3,e.getTipo());
+			stmt.setString(4,e.getDescricao());
+			stmt.executeQuery();
+	}
+	public void Update(Enfermidade e) throws SQLException {
+		Conexao conn = new Conexao();
+		Connection conexao = conn.getConnection();
+		System.out.println(conn.getStatus());
+		String sqlInsere = "UPDATE Enfermidade SET(?,?,?,?)";
+		PreparedStatement stmt = conexao.prepareStatement(sqlInsere);
+			stmt.setInt(1,e.getIdEnfermidade());
+			stmt.setString(2,e.getNome());
+			stmt.setString(3,e.getTipo());
+			stmt.setString(4,e.getDescricao());
+			stmt.execute();
+	}
+	
+}
