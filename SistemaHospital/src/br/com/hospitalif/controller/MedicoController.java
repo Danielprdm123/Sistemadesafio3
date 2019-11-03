13053d3865ca3bbe5bc2879e06dccaf0bdd1b016
Daniel <Daniel@Daniel-PC>
2019-11-03 01:14:25 -0200

    mexendo

--- SistemaHospital/src/br/com/hospitalif/controller/MedicoController.java
+++ SistemaHospital/src/br/com/hospitalif/controller/MedicoController.java
@@ -25,129 +25,135 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class MedicoController  extends Main implements Initializable{
+public class MedicoController extends Main implements Initializable {
 
-    @FXML
-    private TextField txtNumregistro;
+	@FXML
+	private TextField txtNumregistro;
 
-    @FXML
-    private TextField txtEspecialidade;
+	@FXML
+	private TextField txtEspecialidade;
 
-    @FXML
-    private Button btnEnviar;
+	@FXML
+	private Button btnEnviar;
 
-    @FXML
-    private Button btnVolta;
-    @FXML
-    private TextField txtNome;
+	@FXML
+	private Button btnVolta;
+	@FXML
+	private TextField txtNome;
 
-    @FXML
-    private TextField txtCpf;
+	@FXML
+	private TextField txtCpf;
 
-    @FXML
-    private TextField txtIdade;
+	@FXML
+	private TextField txtIdade;
 
-    @FXML
-    private TextField textSangue;
+	@FXML
+	private TextField textSangue;
 
-    @FXML
-    private RadioButton cboSexo;
+	@FXML
+	private RadioButton cboSexo;
 
-    @FXML
-    private ToggleGroup sexo;
+	@FXML
+	private ToggleGroup sexo;
 
-    @FXML
-    private TextArea txtStatus;
+	@FXML
+	private TextArea txtStatus;
 
-    @FXML
-    private TextField txtLogin;
+	@FXML
+	private TextField txtLogin;
 
-    @FXML
-    private PasswordField txtSenha;
+	@FXML
+	private PasswordField txtSenha;
 
-    @FXML
-    private TextArea txtStatusf;
-    
-    @FXML
-    private RadioButton cboSexo1;
-    @FXML
-    private TableView<Medico> ListaMedico;
+	@FXML
+	private TextArea txtStatusf;
 
-    @FXML
-    private TableColumn<Medico, String> TableNome;
+	@FXML
+	private RadioButton cboSexo1;
+	@FXML
+	private TableView<Medico> ListaMedico;
 
-    @FXML
-    private TableColumn<Medico, String> tableIdade;
+	@FXML
+	private TableColumn<Medico, String> TableNome;
 
-    @FXML
-    private TableColumn<Medico, String> tableCpf;
+	@FXML
+	private TableColumn<Medico, String> tableIdade;
 
-    @FXML
-    private TableColumn<Medico, String> tableSangue;
+	@FXML
+	private TableColumn<Medico, String> tableCpf;
 
-    @FXML
-    private TableColumn<Medico, String> tableSexo;
+	@FXML
+	private TableColumn<Medico, String> tableSangue;
 
-    @FXML
-    private TableColumn<Medico, String> tableStaP;
+	@FXML
+	private TableColumn<Medico, String> tableSexo;
 
-    @FXML
-    private TableColumn<Medico, String> tableLogin;
+	@FXML
+	private TableColumn<Medico, String> tableStaP;
 
-    @FXML
-    private TableColumn<Medico, String> tableStaF;
+	@FXML
+	private TableColumn<Medico, String> tableLogin;
 
-    @FXML
-    private TableColumn<Medico, String> tableNumR;
+	@FXML
+	private TableColumn<Medico, String> tableStaF;
 
-    @FXML
-    private TableColumn<Medico, String> tableEspecialidade;
+	@FXML
+	private TableColumn<Medico, String> tableNumR;
 
-    @FXML
-    void Enviarmedico(ActionEvent event) throws SQLException, IOException {
-    	try {
-    	String nome = txtNome.getText();
-    	String cpf = txtCpf.getText();
-    	int idade = Integer.parseInt(txtIdade.getText());
-    	String tipoSangue = textSangue.getText();
-    	String sexo = cboSexo.getText();
-    	String sexo1 = cboSexo1.getText();
-    	String status = txtStatus.getText();
-    	String login = txtLogin.getText();
-    	String senha = txtSenha.getText();
-    	String statusF = txtStatusf.getText();
-    	int NumRegristroMedico = Integer.parseInt(txtNumregistro.getText());
-    	String especialidade = txtEspecialidade.getText();
-    	Medico m = new Medico(); 
-		MedicoDAO  mDAO = new MedicoDAO();
-		m.setNome(nome);
-    	m.setCpf(cpf);
-    	m.setIdade(idade);
-    	m.setTipoSanguineo(tipoSangue);
-    	m.setSexo(sexo);
-    	m.setSexo(sexo1);
-    	m.setStatusPessoa(status);
-    	m.setLogin(login);
-    	m.setSenha(senha);
-    	m.setStatusDeUsuario(statusF);
-		m.setEspecialidade(especialidade);
-		m.setNumeroderegistro(NumRegristroMedico);
-		mDAO.save(m);
-		 openpage(Rotas.DASH);
-    	}catch (NumberFormatException e) {
-    		System.out.println("Campo idade vazio e num registro");
-    		}
-      
-    }
+	@FXML
+	private TableColumn<Medico, String> tableEspecialidade;
+	@FXML
+	private Button remover;
 
-    @FXML
-    void Voltardash(ActionEvent event) throws IOException {
-    	openpage(Rotas.DASH);
+	@FXML
+	private Button atualizar;
 
-    }
-    @Override
+	@FXML
+	void Enviarmedico(ActionEvent event) throws SQLException, IOException {
+		try {
+			String nome = txtNome.getText();
+			String cpf = txtCpf.getText();
+			int idade = Integer.parseInt(txtIdade.getText());
+			String tipoSangue = textSangue.getText();
+			String sexo = cboSexo.getText();
+			String sexo1 = cboSexo1.getText();
+			String status = txtStatus.getText();
+			String login = txtLogin.getText();
+			String senha = txtSenha.getText();
+			String statusF = txtStatusf.getText();
+			int NumRegristroMedico = Integer.parseInt(txtNumregistro.getText());
+			String especialidade = txtEspecialidade.getText();
+			Medico m = new Medico();
+			MedicoDAO mDAO = new MedicoDAO();
+			m.setNome(nome);
+			m.setCpf(cpf);
+			m.setIdade(idade);
+			m.setTipoSanguineo(tipoSangue);
+			m.setSexo(sexo);
+			m.setSexo(sexo1);
+			m.setStatusPessoa(status);
+			m.setLogin(login);
+			m.setSenha(senha);
+			m.setStatusDeUsuario(statusF);
+			m.setEspecialidade(especialidade);
+			m.setNumeroderegistro(NumRegristroMedico);
+			mDAO.save(m);
+			openpage(Rotas.DASH);
+		} catch (NumberFormatException e) {
+			System.out.println("Campo idade vazio e num registro");
+		}
+
+	}
+
+	@FXML
+	void Voltardash(ActionEvent event) throws IOException {
+		openpage(Rotas.DASH);
+
+	}
+
+	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-    	TableNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+		TableNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
 		tableLogin.setCellValueFactory(new PropertyValueFactory<>("login"));
 		tableNumR.setCellValueFactory(new PropertyValueFactory<>("numeroderegistro"));
 		tableSexo.setCellValueFactory(new PropertyValueFactory<>("sexo"));
@@ -160,11 +166,31 @@ public class MedicoController  extends Main implements Initializable{
 		MedicoDAO mdao = new MedicoDAO();
 		List<Medico> medico = mdao.select();
 		ObservableList<Medico> obsm = FXCollections.observableArrayList(medico);
-    	
+
 		ListaMedico.setItems(obsm);
-    	
-    	
-    }
+
+	}
+
+	@FXML
+	void editar(ActionEvent event) {
+
+	}
+
+	@FXML
+	void excluir(ActionEvent event) {
+		try {
+			Medico m = ListaMedico.getSelectionModel().getSelectedItem();
+			MedicoDAO mdao = new MedicoDAO();
+			mdao.removeById(m.getIdFuncionario());
+			openpage(Rotas.MEDICO);
+		} catch (SQLException e) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+
+		}
+
+	}
 
 }
-
--- SistemaHospital/src/br/com/hospitalif/controller/PacienteController.java
+++ SistemaHospital/src/br/com/hospitalif/controller/PacienteController.java
@@ -1,7 +1,5 @@
 package br.com.hospitalif.controller;
 
-
-
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -26,108 +24,112 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+public class PacienteController extends Main implements Initializable {
 
+	@FXML
+	private Button btnCadastrar;
 
-public class PacienteController extends Main implements Initializable{
+	@FXML
+	private TextArea txtHistorico;
 
-    @FXML
-    private Button btnCadastrar;
+	@FXML
+	private TextArea txtDoenca;
 
-    @FXML
-    private TextArea txtHistorico;
+	@FXML
+	private Button btnVoltar;
+	@FXML
+	private TextField txtNome;
 
-    @FXML
-    private TextArea txtDoenca;
+	@FXML
+	private TextField txtCpf;
 
-    @FXML
-    private Button btnVoltar;
-    @FXML
-    private TextField txtNome;
+	@FXML
+	private TextField txtIdade;
 
-    @FXML
-    private TextField txtCpf;
+	@FXML
+	private TextField textSangue;
 
-    @FXML
-    private TextField txtIdade;
+	@FXML
+	private RadioButton cboSexo;
 
-    @FXML
-    private TextField textSangue;
+	@FXML
+	private ToggleGroup sexo;
 
-    @FXML
-    private RadioButton cboSexo;
+	@FXML
+	private TextArea txtStatus;
 
-    @FXML
-    private ToggleGroup sexo;
+	@FXML
+	private RadioButton cboSexo1;
+	@FXML
+	private TableView<Paciente> listaPaiente;
 
-    @FXML
-    private TextArea txtStatus;
-   
-    @FXML
-    private RadioButton cboSexo1;
-    @FXML
-    private TableView<Paciente> listaPaiente;
+	@FXML
+	private TableColumn<Paciente, String> tableNome;
 
-    @FXML
-    private TableColumn<Paciente, String> tableNome;
+	@FXML
+	private TableColumn<Paciente, String> tableIdade;
 
-    @FXML
-    private TableColumn<Paciente, String> tableIdade;
+	@FXML
+	private TableColumn<Paciente, String> tableCpf;
 
-    @FXML
-    private TableColumn<Paciente, String> tableCpf;
+	@FXML
+	private TableColumn<Paciente, String> tableSangue;
 
-    @FXML
-    private TableColumn<Paciente, String> tableSangue;
+	@FXML
+	private TableColumn<Paciente, String> tableSexo;
 
-    @FXML
-    private TableColumn<Paciente, String> tableSexo;
+	@FXML
+	private TableColumn<Paciente, String> tableStaPessoa;
 
-    @FXML
-    private TableColumn<Paciente, String> tableStaPessoa;
+	@FXML
+	private TableColumn<Paciente, String> tableDoenca;
 
-    @FXML
-    private TableColumn<Paciente, String> tableDoenca;
+	@FXML
+	private TableColumn<Paciente, String> tableHistorico;
+	@FXML
+	private Button remover;
 
-    @FXML
-    private TableColumn<Paciente, String> tableHistorico;
-    
-    @FXML
-    void Voltardash(ActionEvent event) throws IOException {
-    	 openpage(Rotas.DASH);
+	@FXML
+	private Button atualizar;
 
-    }
+	@FXML
+	void Voltardash(ActionEvent event) throws IOException {
+		openpage(Rotas.DASH);
 
-    @FXML
-    void cadastrarPaciente(ActionEvent event) throws SQLException, IOException {
-    	try {
-    	String nome = txtNome.getText();
-    	String cpf = txtCpf.getText();
-    	int idade = Integer.parseInt(txtIdade.getText());
-    	String tipoSangue = textSangue.getText();
-    	String sexo = cboSexo.getText();
-    	String sexo1 = cboSexo1.getText();
-    	String status = txtStatus.getText();
-    	String historico = txtHistorico.getText();
-    	String doenca = txtDoenca.getText();
-    	
-    	Paciente p = new Paciente ();
-    	PacienteDAO pDAO = new PacienteDAO();
-    	p.setNome(nome);
-    	p.setCpf(cpf);
-    	p.setIdade(idade);
-    	p.setTipoSanguineo(tipoSangue);
-    	p.setSexo(sexo);
-    	p.setSexo(sexo1);
-    	p.setStatusPessoa(status);
-    	p.setDoenca(doenca);
-    	p.setHistorico(historico);
-    	pDAO.save(p);
-    	 openpage(Rotas.DASH);
-    	}catch (NumberFormatException e) {
-		System.out.println("Campo idade vazio");
+	}
+
+	@FXML
+	void cadastrarPaciente(ActionEvent event) throws SQLException, IOException {
+		try {
+			String nome = txtNome.getText();
+			String cpf = txtCpf.getText();
+			int idade = Integer.parseInt(txtIdade.getText());
+			String tipoSangue = textSangue.getText();
+			String sexo = cboSexo.getText();
+			String sexo1 = cboSexo1.getText();
+			String status = txtStatus.getText();
+			String historico = txtHistorico.getText();
+			String doenca = txtDoenca.getText();
+
+			Paciente p = new Paciente();
+			PacienteDAO pDAO = new PacienteDAO();
+			p.setNome(nome);
+			p.setCpf(cpf);
+			p.setIdade(idade);
+			p.setTipoSanguineo(tipoSangue);
+			p.setSexo(sexo);
+			p.setSexo(sexo1);
+			p.setStatusPessoa(status);
+			p.setDoenca(doenca);
+			p.setHistorico(historico);
+			pDAO.save(p);
+			openpage(Rotas.DASH);
+		} catch (NumberFormatException e) {
+			System.out.println("Campo idade vazio");
 		}
 
-    }
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		tableNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
@@ -138,11 +140,35 @@ public class PacienteController extends Main implements Initializable{
 		tableSangue.setCellValueFactory(new PropertyValueFactory<>("tipoSanguineo"));
 		tableStaPessoa.setCellValueFactory(new PropertyValueFactory<>("statusPessoa"));
 		tableHistorico.setCellValueFactory(new PropertyValueFactory<>("historico"));
-		
+
 		PacienteDAO pdao = new PacienteDAO();
-    	List<Paciente> paciente = pdao.select();
-    	ObservableList<Paciente> obsp = FXCollections.observableArrayList(paciente);
-    	    	
-    	listaPaiente.setItems(obsp);
+		List<Paciente> paciente = pdao.select();
+		ObservableList<Paciente> obsp = FXCollections.observableArrayList(paciente);
+
+		listaPaiente.setItems(obsp);
 	}
+
+	@FXML
+	void editar(ActionEvent event) {
+		
+
+	}
+
+	@FXML
+	void excluir(ActionEvent event) {
+		try {
+			Paciente p = listaPaiente.getSelectionModel().getSelectedItem();
+			PacienteDAO pdao = new PacienteDAO();
+			pdao.removeById(p.getIdPaciente());
+			openpage(Rotas.PACIENTE);
+		} catch (SQLException e) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+
+		}
+
+	}
+
 }
