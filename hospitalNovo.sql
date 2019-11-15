3a4d4d6384f4ce80016d0e9965b041020a48646e
Daniel <Daniel@Daniel-PC>
2019-11-15 01:29:11 -0200

    .

---
+++ hospitalNovo.sql
@@ -0,0 +1,88 @@
+create database hospitalFX;
+use  hospitalFX;
+create table Medico (
+id int not null AUTO_INCREMENT,
+nome varchar(50) ,
+cpf varchar(50) ,
+idade int ,
+tipoSanguineo varchar(50) ,
+sexo varchar(50) ,
+statusPessoa varchar(200), 
+login varchar(50) ,
+senha varchar(50) ,
+statusUsuario varchar(200),
+numeroRegistro int,
+especialidade varchar(50),
+constraint PK_Medico primary key(idMedico));
+
+create table Enfermeiro (
+id int not null AUTO_INCREMENT,
+nome varchar(50) ,
+cpf varchar(50) ,
+idade int ,
+tipoSanguineo varchar(50) ,
+sexo varchar(50) ,
+statusPessoa varchar(200), 
+login varchar(50) ,
+senha varchar(50) ,
+statusUsuario varchar(200),
+numeroRegistro varchar(50),
+constraint PK_Enfermeiro primary key(idEnfermeiro));
+
+create table Gerente (
+id int not null AUTO_INCREMENT,
+nome varchar(50) ,
+cpf varchar(50) ,
+idade int ,
+tipoSanguineo varchar(50) ,
+sexo varchar(50) ,
+statusPessoa varchar(200), 
+login varchar(50) ,
+senha varchar(50) ,
+statusUsuario varchar(200),
+cargo varchar(50),
+constraint PK_Gerente primary key(idGerente));
+
+create table Paciente (
+id int not null AUTO_INCREMENT,
+nome varchar(50) ,
+cpf varchar(50) ,
+idade int ,
+tipoSanguineo varchar(50) ,
+sexo varchar(50) ,
+statusPessoa varchar(200) ,
+doenca varchar (300) ,
+historico varchar (300),
+constraint PK_Paciente primary key(idPaciente));
+
+create table EnfermidadePessoal (
+id int not null AUTO_INCREMENT,
+comentario varchar(100) ,
+statusEnfermeiro varchar(100) ,
+constraint PK_EnfermidadePessoal primary key (idEnfermidadePessoal));
+
+create table Enfermidade(
+id int not null AUTO_INCREMENT,
+nome varchar(50) ,
+tipo varchar(50) ,
+descricao varchar(150) ,
+constraint PK_Enfermidade primary key (idEnfermidade));
+
+create table Entrada (
+id int not null AUTO_INCREMENT,
+dataEntrada date ,
+dataSaida date ,
+statusEntrada varchar(50) ,
+situacaoPaciente varchar(300),
+constraint PK_Entrada primary key (idEntrada));
+
+create table Atendimento (
+id int not null AUTO_INCREMENT,
+comentarioEnfermeiro varchar(100) ,
+comentarioMedico varchar(100) ,
+altura float ,
+peso float ,
+dtData date ,
+doenca varchar(300) ,
+constraint PK_Atendimento primary key (idAtendimento));
+
